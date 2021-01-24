ae1e7ac87320f6998702614d9c657f2150b9d8d5
Nils Jakobi <jakobi.nils@gmail.com>
2021-01-24 22:06:09 +0100

    the interface is now an input. also using error as output now

--- main.go
+++ main.go
@@ -17,14 +17,14 @@ type APICall struct {
 }
 
 // Exec executes the underlying API Call
-func (a APICall) Exec() (i interface{}) {
+func (a APICall) Exec(i interface{}) error {
 	if a.Insecure == true {
 		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 
 	r, err := http.NewRequest(a.Method, a.URL, nil)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if a.Header != nil {
 		// add header to request r
@@ -37,19 +37,18 @@ func (a APICall) Exec() (i interface{}) {
 	resp, err := client.Do(r)
 	if err != nil {
 		// return fmt.Errorf("couldn't get a response with url %s error was %s", url, err)
-		panic(err)
+		return err
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// return fmt.Errorf("couldn't get a response with url %s error was %s", url, err)
-		panic(err)
+		return err
 	}
 
 	err = json.Unmarshal(data, &i)
 	if err != nil {
-		panic(err)
+		return err
 	}
-
-	return i
+	return nil
 }

51cb0790bbf4d2eeefd68c37a1b1bab30132d90a
Nils Jakobi <jakobi.nils@gmail.com>
2021-02-17 12:56:04 +0100

    added cookiejar

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/cookiejar"
 )
 
 // APICall is a type that holds all necessary info for an API call
@@ -15,10 +17,22 @@ type APICall struct {
 	Header   map[string]string
 	Body     []byte
 	Insecure bool
+	Cookie   *http.Cookie
+}
+
+func httpClient() *http.Client {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &http.Client{Jar: jar}
+	return client
 }
 
 // Exec executes the underlying API Call and returns the resulting statuscode and error if any occurred
 func (a APICall) Exec(i interface{}) (statuscode int, err error) {
+	client := httpClient()
+
 	if a.Insecure == true {
 		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	}
@@ -28,6 +42,11 @@ func (a APICall) Exec(i interface{}) (statuscode int, err error) {
 	if err != nil {
 		return 0, err
 	}
+
+	if a.Cookie != nil {
+		r.AddCookie(a.Cookie)
+	}
+
 	if a.Header != nil {
 		// add header to request r
 		for key, value := range a.Header {
@@ -35,7 +54,6 @@ func (a APICall) Exec(i interface{}) (statuscode int, err error) {
 		}
 	}
 
-	client := http.Client{}
 	resp, err := client.Do(r)
 	if err != nil {
 		// return fmt.Errorf("couldn't get a response with url %s error was %s", url, err)
